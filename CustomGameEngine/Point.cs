9793f97f9063c12b11ee2fbc97b3942c4ba28196
Alec Masterson <alecjmasterson@gmail.com>
2025-09-05 21:12:13 -0500

    add basic Point class

---
+++ CustomGameEngine/Point.cs
@@ -0,0 +1,5 @@
+public class Point(float x, float y, float rotation) {
+    public float X { get; set; } = x;
+    public float Y { get; set; } = y;
+    public float Rotation { get; set; } = rotation;
+}
