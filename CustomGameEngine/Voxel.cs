30e42ad25e905c38ca244b7cf2e73e9e10ac190a
Alec Masterson <alecjmasterson@gmail.com>
2025-09-27 07:49:24 -0500

    modify Voxel to track previous state

--- CustomGameEngine/Voxel.cs
+++ CustomGameEngine/Voxel.cs
@@ -10,22 +10,30 @@ public class Voxel(int x, int y) {
     public int ParentId = -1;
 
     public Vector2 Acceleration = Vector2.Zero;
+    public Vector2 AccelerationPrev = Vector2.Zero;
+
     public Vector2 Velocity = Vector2.Zero;
+    public Vector2 VelocityPrev = Vector2.Zero;
 
     public Vector Position = new(x, y);
-    public Vector PositionNext = Vector.Zero;
     public Vector PositionPrev = Vector.Zero;
 
     public Vector2 PositionReal = new(x, y);
-    public Vector2 PositionRealNext = Vector2.Zero;
     public Vector2 PositionRealPrev = Vector2.Zero;
 
     public int Mass = 1;
 
     public void UpdatePhysics(float delta) {
+        AccelerationPrev = Acceleration;
         Acceleration = new Vector2(0, Logic.Gravity) / Mass;
+
+        VelocityPrev = Velocity;
         Velocity += Acceleration * delta;
-        PositionRealNext = PositionReal + (Velocity * delta);
-        PositionNext = new Vector((int) Math.Round(PositionRealNext.X), (int) Math.Round(PositionRealNext.Y));
+
+        PositionRealPrev = PositionReal;
+        PositionReal += Velocity * delta;
+
+        PositionPrev = Position;
+        Position = new Vector((int) Math.Round(PositionReal.X), (int) Math.Round(PositionReal.Y));
     }
 }
